4000ec7835ce3fd56a233ae5e22fda0288c720e5
endeo <endeoddl19@naver.com>
2020-11-14 17:34:44 +0900

    mypage 업적 수정

--- app/src/main/java/com/example/squatchallenge/Mypage.java
+++ app/src/main/java/com/example/squatchallenge/Mypage.java
@@ -2,6 +2,7 @@ package com.example.squatchallenge;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -52,41 +53,48 @@ public class Mypage extends AppCompatActivity {
         ach1 = findViewById(R.id.ach1);
         ach2 = findViewById(R.id.ach2);
         ach3 = findViewById(R.id.ach3);
-        ach1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                select_myach(ach1);
-            }
-        });
-        ach2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                select_myach(ach2);
-            }
-        });
-        ach3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                select_myach(ach3);
-            }
-        });
+        ach1.setOnClickListener(view -> select_myach(ach1));
+        ach2.setOnClickListener(view -> select_myach(ach2));
+        ach3.setOnClickListener(view -> select_myach(ach3));
     }
 
-    public void select_myach(Button button){
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(button.getTextScaleX()==1.05){
-                    button.setTextColor(0xAAFFE607);
-                    button.setBackgroundColor(0xAA575757);
-                    button.setScaleX(1);
-                }
-                else {
-                    button.setTextColor(0xAA575757);
-                    button.setBackgroundColor(0xAAFFE607);
-                    tv_myach.setText(button.getText());
-                    button.setScaleX((float) 1.05);
-                }
+    public void select_myach(Button button){ // 업적 눌렀을때 눌린 버튼만 강조 및 내 업적으로 설정
+        button.setOnClickListener(view -> { // 나머지는 평범하게
+            if(button == findViewById(R.id.ach1)) {
+                button.setTextColor(0xAA575757);
+                button.setBackgroundColor(0xAAFFE607);
+                tv_myach.setText(button.getText());
+                button.setBackgroundResource(R.drawable.buttonshape4);
+                ach2.setTextColor(0xAAFFE607);
+                ach2.setBackgroundColor(0xAA575757);
+                ach2.setBackgroundResource(R.drawable.buttonshape3);
+                ach3.setTextColor(0xAAFFE607);
+                ach3.setBackgroundColor(0xAA575757);
+                ach3.setBackgroundResource(R.drawable.buttonshape3);
+            }
+            else if(button == findViewById(R.id.ach2)) {
+                button.setTextColor(0xAA575757);
+                button.setBackgroundColor(0xAAFFE607);
+                tv_myach.setText(button.getText());
+                button.setBackgroundResource(R.drawable.buttonshape4);
+                ach1.setTextColor(0xAAFFE607);
+                ach1.setBackgroundColor(0xAA575757);
+                ach1.setBackgroundResource(R.drawable.buttonshape3);
+                ach3.setTextColor(0xAAFFE607);
+                ach3.setBackgroundColor(0xAA575757);
+                ach3.setBackgroundResource(R.drawable.buttonshape3);
+            }
+            else {
+                button.setTextColor(0xAA575757);
+                button.setBackgroundColor(0xAAFFE607);
+                tv_myach.setText(button.getText());
+                button.setBackgroundResource(R.drawable.buttonshape4);
+                ach1.setTextColor(0xAAFFE607);
+                ach1.setBackgroundColor(0xAA575757);
+                ach1.setBackgroundResource(R.drawable.buttonshape3);
+                ach2.setTextColor(0xAAFFE607);
+                ach2.setBackgroundColor(0xAA575757);
+                ach2.setBackgroundResource(R.drawable.buttonshape3);
             }
         });
     }
